61a7fe3fa7796e5a43604594c0c74dd31a7e3ab5
SerenaHa12 <serenavander.ha@gmail.com>
2023-12-14 15:12:57 +0700

    fix: deploy error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -32,4 +32,4 @@ module.exports = withStoreConfig({
   },
 })
 
-console.log("next.config.js", JSON.stringify(module.exports, null, 2))
+// console.log("next.config.js", JSON.stringify(module.exports, null, 2))
