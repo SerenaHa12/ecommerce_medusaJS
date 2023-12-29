cd09d85534a5943fbc6e3eb6b576ed17ac6d61a0
SerenaHa12 <serenavander.ha@gmail.com>
2023-12-29 17:16:25 +0700

    fix: staged change bestseller

--- frontend/src/modules/home/components/bestseller/index.tsx
+++ frontend/src/modules/home/components/bestseller/index.tsx
@@ -1,12 +1,19 @@
 "use client"
 
 import React from "react"
-import ProductInfo from "@modules/products/templates/product-info"
+import { useState } from "react"
+import { StoreGetProductsParams } from "@medusajs/medusa"
+import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
+import ProductItem from "./ProductCard"
+import InfiniteProducts from "@modules/products/components/infinite-products"
 
 const BestSellers = () => {
   return (
     <div>
       <h1 className="text-xl-semi">Best Sellers</h1>
+      {/* <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
+        <FeaturedProducts collections={collections} />
+      </Suspense> */}
     </div>
   )
 }
