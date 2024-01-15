f01e45bb7450fa413694094dc7188fcdf4aa23d5
SerenaHa12 <serenavander.ha@gmail.com>
2024-01-15 15:29:27 +0700

    fix: testimodimal image

--- frontend/src/modules/home/components/testimonial/Testimonial.tsx
+++ frontend/src/modules/home/components/testimonial/Testimonial.tsx
@@ -17,7 +17,9 @@ const Testimonial = () => {
                 height={80}
                 alt="dp"
               />
-              <h2 className="text-gray-500 font-black text-[20px]">John Doe</h2>
+              <h2 className="text-gray-500 font-black text-[20px]">
+                Mary Jane
+              </h2>
               <p>CEO & Founder Invision</p>
               <Image
                 className="inline-block py-2"
@@ -33,9 +35,9 @@ const Testimonial = () => {
             </div>
           </div>
 
-          <div className="bg-red-600 bg-[url(/landing/landing01.png)] bg-cover h-[500px] rounded-2xl grid place-items-center">
+          <div className="bg-red-600 bg-[url(/tes.jpg)] bg-cover h-[500px] rounded-2xl grid place-items-center">
             <div className="bg-[#ffffffab] min-w-[270px] sm:min-w-[300px] md:min-w-[500px] rounded-xl py-8 sm:px-8 px-2 grid place-items-center gap-3">
-              <button className="bg-blackish text-white p-2 rounded-md">
+              <button className="bg-blackish text-black p-2 rounded-md">
                 25% DISCOUNT
               </button>
               <h2 className="font-extrabold text-2xl text-[#272727]">
