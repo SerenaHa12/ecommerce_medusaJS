5869b48cbaae8495d791e2acd23017967702103a
tantrinhh <tantrinh0322@gmail.com>
2024-01-23 14:25:21 +0700

    Feat: logic reset SortBy, Collection

--- frontend/src/modules/store/components/refinement-list/collection-filter/index.tsx
+++ frontend/src/modules/store/components/refinement-list/collection-filter/index.tsx
@@ -1,19 +1,29 @@
 import { StoreGetProductsParams } from "@medusajs/medusa"
 import FilterRadioGroup from "@modules/common/components/filter-radio-group"
 import { useCollections } from "medusa-react"
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 
 type SortCollectionFilterProps = {
   refinementList: StoreGetProductsParams
   setRefinementList: (refinementList: StoreGetProductsParams) => void
+  onCollectionSelect: () => void
 }
 
 const CollectionFilter = ({
   refinementList,
   setRefinementList,
+  onCollectionSelect, // Thêm prop mới
 }: SortCollectionFilterProps) => {
   const { collections, isLoading } = useCollections()
   const [collectionId, setCollectionId] = useState<string | null>(null)
+  useEffect(() => {
+    // Cập nhật giá trị collectionId khi refinementList.collection_id thay đổi
+    setCollectionId(
+      refinementList.collection_id && refinementList.collection_id.length > 0
+        ? refinementList.collection_id[0]
+        : null
+    )
+  }, [refinementList.collection_id])
 
   if (!collections) {
     return null
@@ -30,10 +40,14 @@ const CollectionFilter = ({
   ) => {
     setCollectionId(id)
 
-    setRefinementList({
+    const newRefinementList = {
       ...refinementList,
       collection_id: [id],
-    })
+    }
+    setRefinementList(newRefinementList)
+    if (onCollectionSelect) {
+      onCollectionSelect()
+    }
   }
 
   return (
--- frontend/src/modules/store/components/refinement-list/index.tsx
+++ frontend/src/modules/store/components/refinement-list/index.tsx
@@ -2,7 +2,7 @@ import { StoreGetProductsParams } from "@medusajs/medusa"
 import SortProducts, { SortOptions } from "./sort-products"
 import CollectionFilter from "./collection-filter"
 import { Input } from "@medusajs/ui"
-import InfiniteProducts from "@modules/products/components/infinite-products"
+import { useEffect, useState } from "react"
 
 type RefinementListProps = {
   refinementList: StoreGetProductsParams
@@ -13,6 +13,7 @@ type RefinementListProps = {
   setSearchValue: (value: string) => void // Thêm setSearchValue vào props
   searchValue: string
 }
+
 const RefinementList = ({
   refinementList,
   setRefinementList,
@@ -22,6 +23,42 @@ const RefinementList = ({
   setSearchValue,
   searchValue,
 }: RefinementListProps) => {
+  const [clearFiltersActive, setClearFiltersActive] = useState(false)
+  const [appliedFiltersActive, setAppliedFiltersActive] = useState(false)
+  useEffect(() => {
+    if (
+      typeof !refinementList === "undefined" ||
+      typeof refinementList.collection_id === "undefined" ||
+      refinementList.collection_id.length === 0 ||
+      refinementList.collection_id[0] === null
+    )
+      if (sortBy) {
+        setClearFiltersActive(false) // Đặt màu clearFilters thành false khi đã chọn sortBy
+        setAppliedFiltersActive(true)
+      } else {
+        setClearFiltersActive(true)
+        setAppliedFiltersActive(false)
+      }
+  }, [sortBy, refinementList.collection_id])
+  const handleClearFiltersClick = () => {
+    setClearFiltersActive(true)
+    setAppliedFiltersActive(false)
+    setSortBy(false)
+    setRefinementList({
+      ...refinementList,
+      collection_id: [], // Đặt giá trị collection_id về rỗng khi nhấn Clear All Filters
+    })
+  }
+
+  const handleAppliedFiltersClick = () => {
+    setAppliedFiltersActive(true)
+    setClearFiltersActive(false)
+    // Check if setSortBy is selected
+    if (typeof setSortBy === "function") {
+      setSortBy(true)
+    }
+  }
+
   return (
     <>
       <div className="flex flex-col md:24  gap-12 px-8  mb-8 small:pr-0 small:pl-8 small:min-w-[350px] small:ml-[1.675rem]">
@@ -41,11 +78,29 @@ const RefinementList = ({
           <CollectionFilter
             refinementList={refinementList}
             setRefinementList={setRefinementList}
+            onCollectionSelect={() => setAppliedFiltersActive(true)}
           />
         )}
+        <div className="flex gap-5 w-full justify-between">
+          <div
+            className={`cursor-pointer px-4 py-2 ${
+              clearFiltersActive ? "bg-[#5A6D57]" : ""
+            }`}
+            onClick={handleClearFiltersClick}
+          >
+            Clear All Filters
+          </div>
+          <div
+            className={`cursor-pointer px-4 py-2 ${
+              appliedFiltersActive ? "bg-[#5A6D57]" : ""
+            }`}
+            // onClick={handleAppliedFiltersClick}
+          >
+            Applied filters
+          </div>
+        </div>
       </div>
     </>
   )
 }
-
 export default RefinementList
