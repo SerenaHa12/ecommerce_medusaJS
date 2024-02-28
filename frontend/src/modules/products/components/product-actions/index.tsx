e61e75366da998c69f55c1a236f7ce9a88ed7ba2
SerenaHa12 <serenavander.ha@gmail.com>
2024-02-28 13:57:39 +0700

    fix: rewrite product details, change product details image

--- frontend/src/modules/products/components/product-actions/index.tsx
+++ frontend/src/modules/products/components/product-actions/index.tsx
@@ -19,22 +19,16 @@ const ProductActionsInner: React.FC<ProductActionsProps> = ({ product }) => {
   const { updateOptions, addToCart, options, inStock, variant } =
     useProductActions()
 
-  // const price = useProductPrice({ id: product.id!, variantId: variant?.id })
+  const price = useProductPrice({ id: product.id!, variantId: variant?.id })
 
-  // const selectedPrice = useMemo(() => {
-  //   const { variantPrice, cheapestPrice } = price
+  const selectedPrice = useMemo(() => {
+    const { variantPrice, cheapestPrice } = price
 
-  //   return variantPrice || cheapestPrice || null
-  // }, [pri
+    return variantPrice || cheapestPrice || null
+  }, [price])
+
   return (
-    <div className="flex flex-col gap-y-2 w-full">
-      <div className="flex flex-col gap-8 mb-32  max-md:text-center max-md:items-center">
-        <div className="text-[32px] font-semibold">Wrap Top</div>
-        <div>
-          Versatile And Universally Flattering, Our Wrap Blouse Can Be Tied,
-          Draped, Snapped And Wrapped Multiple Ways.
-        </div>
-      </div>
+    <div className="flex flex-col gap-y-2">
       <div>
         {product.variants.length > 1 && (
           <div className="flex flex-col gap-y-4">
@@ -55,7 +49,7 @@ const ProductActionsInner: React.FC<ProductActionsProps> = ({ product }) => {
         )}
       </div>
 
-      {/* {selectedPrice ? (
+      {selectedPrice ? (
         <div className="flex flex-col text-ui-fg-base">
           <span
             className={clsx("text-xl-semi", {
@@ -80,13 +74,13 @@ const ProductActionsInner: React.FC<ProductActionsProps> = ({ product }) => {
         </div>
       ) : (
         <div></div>
-      )} */}
+      )}
 
       <Button
         onClick={addToCart}
         disabled={!inStock || !variant}
         variant="primary"
-        className=" h-10 bg-[#AA1020] text-white rounded-full px-6 font-semibold"
+        className="w-full h-10"
       >
         {!inStock
           ? "Out of stock"
@@ -94,41 +88,6 @@ const ProductActionsInner: React.FC<ProductActionsProps> = ({ product }) => {
           ? "Select variant"
           : "Add to cart"}
       </Button>
-      <div className="flex justify-between text-[#868686] px-4 py-8">
-        <div className="flex items-center gap-2">
-          <div>
-            <BsTruck className="w-5 h-5" />
-          </div>
-          <div>Easy Return</div>
-        </div>
-        {/* <div className="flex items-center gap-2">
-          <div>
-            <CiHeart className="w-5 h-5" />
-          </div>
-          <div>Add To Wish List</div>
-        </div> */}
-      </div>
-      {/* <div className="border-2 border-[#ADADAD] px-4 py-8 bg-[#F0F2EF] flex flex-col gap-8">
-        <div className="pb-4 text-xl font-normal leading-9 border-b border-[#ADADAD]">
-          Silk
-        </div>
-        <div className="text-lg leading-8 font-normal">
-          This Material Is Our Signature High-Stretch Fabric That Drapes Like
-          Silk And Is Soft To The Touch. Silk Is OEKO-TEX® Certified And Made In
-          Italy In A Mill 100% Powered By Renewable Energy (Solar And Biomass)
-        </div>
-        <div className="flex gap-4 text-sm font-normal leading-7 mt-5">
-          <div className="py-3 px-4 my-auto bg-white cursor-pointer">
-            Quick Dry
-          </div>
-          <div className="max-w-[190px] w-full text-center my-auto py-3 bg-white cursor-pointer">
-            Chemical Free
-          </div>
-          <div className="max-w-[183px] w-full text-center my-auto py-3 bg-white cursor-pointer">
-            Machine Washable
-          </div>
-        </div>
-      </div> */}
     </div>
   )
 }
